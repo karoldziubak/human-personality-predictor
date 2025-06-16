0b539a0c64f741a12687439ea10df1de74a5cb11
karoldziubak <karoldziubak@gmail.com>
2025-06-16 18:43:26 +0200

    feat: add LightGBM model implementation

---
+++ src/models/ensemble_lightgbm.py
@@ -0,0 +1,18 @@
+
+from src.models import BaseModel
+from sklearn.model_selection import RandomizedSearchCV
+from lightgbm import LGBMClassifier
+
+class LightGBMModel(BaseModel):
+    def __init__(self, param_grid=None):
+        super().__init__(name="LightGBM")
+        self.param_grid = param_grid
+
+    def build_model(self):
+        base_model = LGBMClassifier(random_state=self.random_state)
+        
+        # Hyperparameter tuning
+        if self.param_grid:
+            self.model = RandomizedSearchCV(estimator=base_model, param_distributions=self.param_grid, n_iter=20, scoring='f1_weighted', cv=self.cv, n_jobs=-1, random_state=self.random_state)
+        else:
+            self.model = base_model
--- src/models/paramaters.py
+++ src/models/paramaters.py
@@ -1,6 +1,7 @@
 from src.models.ensemble_random_forest import RandomForestModel
 from src.models.baseline_logistic_regression import LogisticRegressionModel
 from src.models.ensemble_xgboost import XGBoostModel
+from src.models.ensemble_lightgbm import LightGBMModel
 import numpy as np
 
 model_parameters = {
@@ -10,6 +11,10 @@ model_parameters = {
         },
     'xgboost': {
         'model': XGBoostModel(param_grid={'n_estimators': [100, 150, 200, 250, 300], 'learning_rate': [0.01, 0.05, 0.1, 0.2, 0.3], 'max_depth': [None, 2, 4, 6, 8, 10], 'subsample': [0.5, 0.8, 1.0]}),
+        'scaled': False,
+        },
+    'light_gbm': {
+        'model': LightGBMModel(param_grid={'n_estimators': [100, 150, 200, 250, 300], 'learning_rate': [0.01, 0.05, 0.1, 0.2, 0.3], 'max_depth': [None, 2, 4, 6, 8, 10], 'subsample': [0.5, 0.8, 1.0], 'colsample_bytree': [0.5, 0.8, 1.0], 'num_leaves': np.linspace(15, 100, 5).astype(int).tolist(), 'verbose': [-1]}),
         'scaled': False,
         },
     'logistic_regression': {
