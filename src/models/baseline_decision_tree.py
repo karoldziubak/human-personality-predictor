a93380ebfc8c6f081036a2e4e477a92e66019a7e
karoldziubak <karoldziubak@gmail.com>
2025-06-24 19:32:02 +0200

    feat: add DecisionTree model implementation

---
+++ src/models/baseline_decision_tree.py
@@ -0,0 +1,17 @@
+from src.models import BaseModel
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.tree import DecisionTreeClassifier
+
+class DecisionTreeModel(BaseModel):
+    def __init__(self, param_grid=None):
+        super().__init__(name="Decision Tree")
+        self.param_grid = param_grid
+
+    def build_model(self):
+        self.model = DecisionTreeClassifier(random_state=self.random_state)
+
+        # Hyperparameter tuning
+        if self.param_grid:
+            self.model = RandomizedSearchCV(estimator=self.model, param_distributions=self.param_grid, n_iter=self.n_iter, scoring='f1_weighted', cv=self.cv, n_jobs=-1, random_state=self.random_state)
+        else:
+            self.model = self.model
--- src/models/paramaters.py
+++ src/models/paramaters.py
@@ -3,11 +3,12 @@ from src.models.baseline_logistic_regression import LogisticRegressionModel
 from src.models.ensemble_xgboost import XGBoostModel
 from src.models.ensemble_lightgbm import LightGBMModel
 from src.models.ensemble_catboost import CatBoostModel
+from src.models.baseline_decision_tree import DecisionTreeModel
 import numpy as np
 
 model_parameters = {
     'random_forest': {
-        'model': RandomForestModel(param_grid={'n_estimators': [100, 150, 200, 250, 300], 'max_depth': [None, 2, 4, 6, 8, 10], 'min_samples_split': [5, 7, 9, 11]}),
+        'model': RandomForestModel(param_grid={'n_estimators': [100, 150, 200, 250, 300], 'max_depth': [None, 2, 4, 6, 8, 10], 'min_samples_split': [3, 5, 7, 9, 11]}),
         'scaled': False,
         },
     'xgboost': {
@@ -25,5 +26,9 @@ model_parameters = {
     'logistic_regression': {
         'model': LogisticRegressionModel(param_grid={'C': np.logspace(-4, 4, 20), 'solver': ['lbfgs','liblinear']}),
         'scaled': True,
+        },
+    'decision_tree': {
+        'model': DecisionTreeModel(param_grid={'criterion': ['gini', 'entropy', 'log_loss'], 'max_depth': [None, 2, 4, 6, 8, 10], 'min_samples_split': [3, 5, 7, 9, 11], 'min_samples_leaf': [1, 3, 5, 7] ,'max_features': [None, 'sqrt', 'log2'], 'ccp_alpha': [0.0, 0.01, 0.02, 0.05, 0.1]}),
+        'scaled': False,
         },
 }
